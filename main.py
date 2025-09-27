4bcbe73b04922201aa2f63b3a930535a0a4da69b
ItchyTrack <benherman345@gmail.com>
2025-09-26 21:06:12 -0400

    fixed because cam broke this

--- main.py
+++ main.py
@@ -36,12 +36,7 @@ for index, row in data2.iterrows():
 		# sec.pop("credMin")
 		data3 = pd.concat((data3, pd.DataFrame(sec)))
 
-# data3 = data3.drop_duplicates(subset='crn')
-# print(data3.head(50))
-
 import networkx as nx
-
-import matplotlib.pyplot as plt
 
 G = nx.Graph()
 for index, row in data3.iterrows():
@@ -78,22 +73,27 @@ for sub in cpy:
 	found = 0
 	key = list()
 	credDict = {}
+	hasBioLab = False
+	needsBioLab = False
 	for c in sub:
 		row = data3.loc[data3['crn'] == c].iloc[0]
+		if (row["title"] == "Introduction To Biology"):
+			needsBioLab = True
+		elif (row["title"] == "Intro Biol Computatinal Lab" or row["title"] == "Introduction To Biology Laboratory"):
+			hasBioLab = True
 		if (row["title"] in needed):
 			found += 1
 		# print(row)
 		credMax += row["credMax"]
 		credMin += row["credMin"]
-		credDict[row["crse"]] = (int(row["credMin"]), int(row["credMax"]))
-		key.append(row["crse"])
-	if not (found == len(needed) and credMax >= filterData["cred"]["min"] and credMin <= filterData["cred"]["max"]):
+		credDict[row['subj'] + str(row["crse"])] = (int(row["credMin"]), int(row["credMax"]))
+		key.append(row["subj"] + str(row["crse"]))
+	if not ((needsBioLab == hasBioLab) and found == len(needed) and credMax >= filterData["cred"]["min"] and credMin <= filterData["cred"]["max"]):
 		subs.remove(sub)
 	else:
 		key.sort()
 		credData[str(key)] = credDict
 
-# print(subs)
 typeSets = {}
 for sub in subs:
 	data = set()
@@ -101,7 +101,7 @@ for sub in subs:
 	for c in sub:
 		row = data3.loc[data3['crn'] == c].iloc[0]
 		data.add(row["title"])
-		key.append(row["crse"])
+		key.append(row["subj"] + str(row["crse"]))
 	key.sort()
 	if str(key) not in typeSets:
 		typeSets[str(key)] = {}
@@ -122,21 +122,24 @@ maxEnd = 0
 for ds in typeSets:
 	rowsToPrint.append([])
 	for d in ds[1:-1].split(", "):
-		row = data3.loc[data3['crse'] == int(d)].iloc[0]
-		start = d + "      " + row["title"]
+		d = d[1:-1]
+		crse = int(''.join(x for x in d if x.isdigit()))
+		subj = ''.join(x for x in d if not x.isdigit())
+		row = data3.loc[(data3['crse'] == crse) & (data3['subj'] == subj)].iloc[0]
+		start = subj + "      " + str(crse) +"      " + row["title"]
 		endList = list(typeSets[ds][row["title"]])
 		end = ""
 		for i in endList[:-1]:
 			end += str(i) + ", "
 		end += str(endList[-1])
-		rowsToPrint[-1].append((credData[ds][int(d)], start, end))
+		rowsToPrint[-1].append((credData[ds][row["subj"] + str(row["crse"])], start, end))
 		if len(start) > maxStart:
 			maxStart = len(start)
 		if len(end) > maxEnd:
 			maxEnd = len(end)
 
 lenOfLine = maxStart + 5 + maxEnd+14
-print("Cred l:h     CRSE:     Title:" +  " "*(maxStart + 5-16) + "CRIs:")
+print("Cred l:h     SUBJ:     CRSE:     Title:" +  " "*(maxStart + 5-26) + "CRIs:")
 print("-"*(lenOfLine))
 for rows in rowsToPrint:
 	sumL = 0
@@ -147,32 +150,3 @@ for rows in rowsToPrint:
 		print(str(cred[0]) + ":" + str(cred[1])+ " "*(13-len(str(int(cred[0])) + ":" + str(int(cred[1]))))+ start + " "*(maxStart + 5 -len(start)) + end)
 	print(str(sumL) + ":" + str(sumH))
 	print("-"*(lenOfLine))
-
-# print(subs)
-# for sub in subs:
-# 	data = []
-# 	for c in sub:
-# 		row = data3.loc[data3['crn'] == c].iloc[0]
-# 		data.append((row["title"], int(c)))
-# 	data.sort()
-# 	for d in data:
-# 		print(d[0], str(d[1]) + ", ", end="")
-# 	print()
-# 	# nx.draw_networkx_edges(G, pos)
-
-# exit()
-
-# for sub in subs:
-# 	pos = nx.spring_layout(G)
-# 	nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'),  node_size = 500, nodelist=sub)
-# 	nx.draw_networkx_labels(G, pos)
-# 	nx.draw_networkx_edges(G, pos)
-# 	print(sub)
-# 	credMax = 0
-# 	credMin = 0
-# 	foundDif = False
-# 	for c in sub:
-# 		row = data3.loc[data3['crn'] == c].iloc[0]
-# 		print(row["title"], row["credMax"])
-# 	# nx.draw_networkx_edges(G, pos)
-# 	plt.show()
